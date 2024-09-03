eb85b44b6cdca62b10d0c9f52fb737e03b87470b
Caelest1s <jefferson.crn@gmail.com>
2024-09-03 17:52:23 -0300

    build: Spring Web, H2, Spring JPA, Spring actuator, Spring DevTools, Lombok, Validation

---
+++ api/src/test/java/com/caelestis/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caelestis.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
