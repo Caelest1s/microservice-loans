c29a15d9d93a713a8015717463421f3890b0b74c
Caelest1s <jefferson.crn@gmail.com>
2024-09-04 02:23:01 -0300

    feat: LoanRepository

---
+++ api/src/main/java/com/caelestis/loans/repository/LoanRepository.java
@@ -0,0 +1,13 @@
+package com.caelestis.loans.repository;
+
+import com.caelestis.loans.entity.Loans;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loans, Long> {
+
+    Optional<Loans> findByMobileNumber(String mobileNumber);
+}
