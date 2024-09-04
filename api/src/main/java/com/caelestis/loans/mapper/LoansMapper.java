f69705f0130b84a0f9a5947113bd343f5b18cc01
Caelest1s <jefferson.crn@gmail.com>
2024-09-04 02:16:54 -0300

    feat: Entity's, DTO, schema.sql and application.yml

---
+++ api/src/main/java/com/caelestis/loans/mapper/LoansMapper.java
@@ -0,0 +1,27 @@
+package com.caelestis.loans.mapper;
+
+import com.caelestis.loans.dto.LoansDto;
+import com.caelestis.loans.entity.Loans;
+
+public class LoansMapper {
+
+    public static LoansDto mapToLoanDto(Loans loans, LoansDto loansDto) {
+        loansDto.setMobileNumber(loans.getMobileNumber());
+        loansDto.setLoanNumber(loans.getLoanNumber());
+        loansDto.setLoanType(loans.getLoanType());
+        loansDto.setTotalLoan(loans.getTotalLoan());
+        loansDto.setAmountPaid(loans.getAmountPaid());
+        loansDto.setOutstandingAmount(loans.getOutstandingAmount());
+        return loansDto;
+    }
+
+    public static Loans mapToLoan(LoansDto loansDto, Loans loans) {
+        loans.setMobileNumber(loansDto.getMobileNumber());
+        loans.setLoanNumber(loansDto.getLoanNumber());
+        loans.setLoanType(loansDto.getLoanType());
+        loans.setTotalLoan(loansDto.getTotalLoan());
+        loans.setAmountPaid(loansDto.getAmountPaid());
+        loans.setOutstandingAmount(loansDto.getOutstandingAmount());
+        return loans;
+    }
+}
